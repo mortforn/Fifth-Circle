d68f4360f6c75100b510f4063b54367d03aaa1e4
Matt Fine <mfine@wesleyan.edu>
2019-02-04 12:30:40 -0500

    continue deployment

--- server/server.js
+++ server/server.js
@@ -45,6 +45,19 @@ app.use(generateMusic)
 app.use(userRoutes)
 app.use(intervalRoute)
 
+if (process.env.NODE_ENV === 'production') {
+  // Express will serve up production assets
+  // like our main.js file, or main.css file!
+  app.use(express.static('client/build'));
+
+  // Express will serve up the index.html file
+  // if it doesn't recognize the route
+  const path = require('path');
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
+  });
+}
+
 server.listen(PORT, () => {
   console.log('Node.js listening on port ' + PORT)
 })
